01225f6148f3f8b4e50ac3b24f881a00b7f1fa9b
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-11-17 20:03:53 -0300

    review of class.

--- Entities/Invoice.cs
+++ Entities/Invoice.cs
@@ -6,14 +6,14 @@ public class Invoice(double basicPayment, double tax) {
     private double BasicPayment { get; set; } = basicPayment;
     private double Tax { get; set; } = tax;
 
-    public double TotalPyment => BasicPayment + Tax;
+    private double TotalPayment => BasicPayment + Tax;
 
     public override string ToString()
     {
         return "=============INVOICE=============" + "\n" +
                "Basic payment:" + BasicPayment.ToString("F2", CultureInfo.InvariantCulture) + "\n" +
                "Tax: " + Tax.ToString("F2", CultureInfo.InvariantCulture) + "\n" +
-               "Total payment: " + TotalPyment.ToString("F2", CultureInfo.InvariantCulture) + "\n" +
+               "Total payment: " + TotalPayment.ToString("F2", CultureInfo.InvariantCulture) + "\n" +
                "=============INVOICE=============";
     }
 }
--- Services/BrazilTaxService.cs
+++ Services/BrazilTaxService.cs
@@ -1,15 +1,9 @@
 namespace ConsoleInterface.Services;
 
-public class BrazilTaxService
-{
-    public double Tax(double amount)
-    {
-        if (amount<= 100)
-        {
+public class BrazilTaxService {
+    public  double Tax(double amount) {
+        if (amount <= 100)
             return amount * 0.2;
-
-        }
         return amount * 0.15;
-
     }
 }
